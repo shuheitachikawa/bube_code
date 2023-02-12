4915b5dbeaa46454c42c5cd39fd42ae1ff1fed32
shuheitachikawa <shuhei.tusx@gmail.com>
2023-02-12 12:23:11 +0900

    s

--- javascript/ブラウザ操作/index.js
+++ javascript/ブラウザ操作/index.js
@@ -1,39 +1,13 @@
-location.reload();
 
-const copyText = "コピーしたい文字列";
+const logo = document.querySelector(".lnXdpd");
 
-navigator.clipboard.writeText(copyText);
+let deg = 0;
 
-navigator.mediaDevices.getUserMedia({video: true})
+const rotate = () => {
+  logo.style.transform = `rotate(${deg}deg)`;
+  deg++;
+}
 
-const text = "おはよう";
-
-speechSynthesis.speak(new SpeechSynthesisUtterance(text));
-
-
-const title = "表示したいタイトル";
-const url = "シェアしたいURL";
-
-navigator.share({ title, url }) 
-
-alert("こんにちは");
-
-const confirmed = confirm("実行しますか？");
-
-navigator.userAgent
-// -> 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-
-
-navigator.share({ title, url }) 
-// ↓本来は
-window.navigator.share({ title, url }) 
-
-location.reload();
-// ↓本来は
-window.location.reload();
-
-console.log("こんにちは");
-// ↓本来は
-window.console.log("こんにちは");
+setInterval(rotate, 10);
 
 
