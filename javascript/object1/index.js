4057ad1c573f36222d9be2f45dd3c79f66939f39
shuheitachikawa <shuhei.tusx@gmail.com>
2023-05-03 22:12:34 +0900

    s

--- javascript/object1/index.js
+++ javascript/object1/index.js
@@ -1,49 +1,51 @@
-const name = "ぶべ"; // 年齢
+// const name = "ぶべ"; // 年齢
 
-const age = "2"; // 年齢
+// // const age = "2"; // 年齢
 
-// 私の猫
+// // 私の猫
+// // const myCat = {
+// //   name: "ぶべ",
+// //   age: 2,
+// //   favoriteFood: "魚",
+// //   color: "black"
+// // };
+
+// // ドットアクセス
+// console.log(myCat.name); // "ぶべ"
+
+// // keyアクセス
+// console.log(myCat["name"]) // "ぶべ"
+
+// // 私の猫
+// // const myCat2 = {
+// //   name: "ぶべ",
+// //   age: 2,
+// //   favoriteFoods: ["魚", "チーズ", "チュール"],
+// //   greet: function() {
+// //     console.log("にゃー");
+// //   }
+// // };
+
+// console.log(myCat2.favoriteFoods[0]); // "魚"
+
+// myCat2.greet(); // "にゃー"
+
+
 const myCat = {
   name: "ぶべ",
   age: 2,
-  favoriteFood: "魚",
-  color: "black"
 };
 
-// ドットアクセス
-console.log(myCat.name); // "ぶべ"
+// key名のリストを配列として取得
+Object.keys(myCat); // ["name", "age"]
 
-// keyアクセス
-console.log(myCat["name"]) // "ぶべ"
+// valueのリストを配列として取得
+Object.values(myCat); // ["ぶべ", 2]
 
-// 私の猫
-const myCat2 = {
-  name: "ぶべ",
-  age: 2,
-  favoriteFoods: ["魚", "チーズ", "チュール"],
-  greet: function() {
-    console.log("にゃー");
-  }
-};
+// keyとvalueのリストを配列として取得
+Object.entries(myCat); // [["name", "ぶべ"], ["age", 2]]
 
-console.log(myCat2.favoriteFoods[0]); // "魚"
-
-myCat2.greet(); // "にゃー"
-
-
-// 私の猫
-const me = {
-  name: "ぶべコード",
-  cat: {
-    name: "ぶべ",
-    age: 2,
-    favoriteFood: {
-      name: "ちゅーる",
-      price: 100
-    }
-  }
-};
-
-// 私の猫の一番好きな食べ物の値段
-console.log(me.cat.favoriteFood.price); // 100
+// スプレッド構文を使った値の取得
+const { name, age } = myCat;
+console.log(age); // 2
 
