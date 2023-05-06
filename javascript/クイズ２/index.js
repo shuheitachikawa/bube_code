80e65c7ff4b9781c3b776bbdbacc96c8db7f504b
shuhei.tachikawa <shuhei.tusx@gmail.com>
2023-05-06 12:07:10 +0900

    s

---
+++ javascript/クイズ２/index.js
@@ -0,0 +1,21 @@
+const x = 1;
+
+if (x === 1) {
+  console.log("こんにちは");
+}
+
+// 1
+x === 1 && console.log("こんにちは");
+
+// 2
+x === 1 || console.log("こんにちは");
+
+// 3
+x === 1 ?? console.log("こんにちは");
+
+// 4
+x === 1 and console.log("こんにちは");
+
+
+
+
